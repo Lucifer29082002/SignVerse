3a84797db52e33887ee42474c468796a22225036
abdulrakib <firefalls2004@gmail.com>
2024-09-17 17:38:24 +0530

    created homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,83 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { useState } from 'react';
+import { IonApp, IonContent, IonModal, IonButton, IonIcon } from '@ionic/react';
+import { cameraOutline, micOutline, closeOutline, sendOutline } from 'ionicons/icons'; // Import send icon
 import Home from './pages/Home';
 import CameraPage from './pages/CameraPage/CameraPage';
+import MicPage from './pages/MicPage/micpage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Mic from './pages/MicPage/micpage';
 
-setupIonicReact();
+/* Import setupIonicReact */
+import { setupIonicReact } from '@ionic/react';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home"><Home /></Route>
-        <Route exact path="/camera" component={CameraPage} />
-        <Route exact path="/mic" component={Mic} />
-        <Route exact path="/"><Redirect to="/home" /></Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+setupIonicReact(); // Initialize Ionic React
+
+const App: React.FC = () => {
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [isMicOpen, setIsMicOpen] = useState(false);
+
+  return (
+    <IonApp>
+      <IonContent>
+        <Home />
+
+        {/* Placeholder for the model with an image */}
+        <div className="model-placeholder">
+          <img src="https://via.placeholder.com/150" alt="Model Placeholder" />
+        </div>
+
+        {/* Icon Containers */}
+        <div className="icon-container">
+          <div className="camera-icon-container">
+            <IonButton onClick={() => setIsCameraOpen(true)}>
+              <IonIcon icon={cameraOutline} />
+            </IonButton>
+          </div>
+          <div className="mic-icon-container">
+            <IonButton onClick={() => setIsMicOpen(true)}>
+              <IonIcon icon={micOutline} />
+            </IonButton>
+          </div>
+        </div>
+
+        {/* Bottom Input Box */}
+        <div className="bottom-input-box">
+          <input type="text" placeholder="Enter text here..." />
+          
+          {/* Arrow Button */}
+          <IonButton className="submit-button">
+            <IonIcon icon={sendOutline} />
+          </IonButton>
+        </div>
+
+        {/* Camera Modal */}
+        <IonModal isOpen={isCameraOpen} onDidDismiss={() => setIsCameraOpen(false)}>
+          <CameraPage />
+          <IonIcon className="close-icon" icon={closeOutline} onClick={() => setIsCameraOpen(false)} />
+        </IonModal>
+
+        {/* Mic Modal */}
+        <IonModal isOpen={isMicOpen} onDidDismiss={() => setIsMicOpen(false)}>
+          <MicPage />
+          <IonIcon className="close-icon" icon={closeOutline} onClick={() => setIsMicOpen(false)} />
+        </IonModal>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
